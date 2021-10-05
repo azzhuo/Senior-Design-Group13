433dcf46a31e66d63b3ef78f033c7e8a6e1c66e0
azzhuo <57459595+azzhuo@users.noreply.github.com>
2021-10-05 14:05:02 -0400

    Update GameStateSensor.m
    add struct for identified objects

--- Project 2/GameStateSensor.m
+++ Project 2/GameStateSensor.m
@@ -84,7 +84,11 @@ figure();
 imshow(oppo);
 title('Colors');
 
-
+%% struct for identifying colors and location
+gameState.wellLoc=[30,60,90,135,180,-45,-90,-120,-150]; %All of the well locations, length = n
+gameState.wellColor=[0,0,1,0,2,0,0,4,0]; % Color of stickers in the desired configuration,
+% Length = n,colors described in "key" 
+gameState.key={'0', 'Empty';'1', 'Red'; '2', 'Green'; '3', 'Yellow'; '4','Blue'}; %Key
 
 
 
